e304b7db3b7b634634f9b5a1b35f299eb766339f
emartinezf <emartinez@e-healthtracker.com>
2020-09-28 21:30:43 -0500

    Se agrego GET /posts/:postId/comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { getPostComments } = require('./routers/comments-router');
 
 const app = express();
 app.get('/', (req, res) => {
     console.log('Serving root resource');
     res.send('Hello world!!!');
 });
+
+app.get('/posts/:postId/comments', getPostComments);
 
 app.listen(3000, () => {
     console.log(`Express is up and running`);
