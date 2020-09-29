2d66c98df20ee8e6a240632680a09b6f64ce00ab
emartinezf <emartinez@e-healthtracker.com>
2020-09-28 20:38:06 -0500

    Se creo un hola mundo con express
     On branch ft/bootstrap
     Changes to be committed:
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+    console.log('Serving root resource');
+    res.send('Hello world!!!');
+});
+
+app.listen(3000, () => {
+    console.log(`Express is up and running`);
+})
