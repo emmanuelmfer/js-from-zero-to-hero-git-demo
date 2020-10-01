bb3bad2720fe758d653df5c9aaf062d971e5fba4
vamps666 <obnsp.angel@gmail.com>
2020-09-30 21:21:21 -0500

    Se agrego post /comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { getPostComments } = require('./routers/comments-router');
+const { getPostComments, postComment } = require('./routers/comments-router');
 
 const app = express();
+
+app.use(express.json());
 app.get('/', (req, res) => {
     console.log('Serving root resource');
     res.send('Hello world!!!');
 });
 
 app.get('/posts/:postId/comments', getPostComments);
+app.post('/comments', postComment);
 
 app.listen(3000, () => {
     console.log(`Express is up and running`);
--- src/samples/comments.js
+++ src/samples/comments.js
@@ -2,17 +2,20 @@ const comments = {
     1: [
         {
             "content": "Eso es demasiado gay",
-            "author": "Emmanuel Martinez"
+            "author": "Emmanuel Martinez",
+            "postId": 1
         },
         {
             "content": "Que bonito es estudiar con Emmanuel",
-            "author": "Angel Fernandez"
+            "author": "Angel Fernandez",
+            "postId": 1
         }
     ],
     2: [
         {
             "content": "Eso es demasiado macho",
-            "author": "Angel Audelo"
+            "author": "Angel Audelo",
+            "postId": 2
         }
     ]
 };
